0ac849e78f10bd47c3de0e93c75fc946715d9b53
unknown <coder.rakibul@gmail.com>
2022-06-18 00:37:54 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Products from './pages/Products/Products';
+import Header from './pages/Shared/Header';
 
 function App() {
   return (
     <div>
-      <h2 className='text-primary'>Project Started</h2>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
