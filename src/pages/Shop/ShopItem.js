208fd51c9abcd4b86ac5346b325b723005a2650c
unknown <coder.rakibul@gmail.com>
2022-06-18 13:20:36 +0600

    modal added

--- src/pages/Shop/ShopItem.js
+++ src/pages/Shop/ShopItem.js
@@ -8,26 +8,23 @@ const ShopItem = ({ shoe }) => {
 
             <label for="description-modal" class="btn modal-button">Description</label>
             <figure class="px-10 pt-4">
-                <img className='h-48' src={image} alt="Shoes" class="rounded-xl" />
+                <img className='h-24' src={image} alt="Shoes" class="rounded-xl" />
             </figure>
             <div class="card-body">
                 <h2>Brand: {name}</h2>
                 <p>Code: {model}</p>
-                <h3>Price: ${price}</h3>
+                <h3 className='text-xl font-bold'>Price: <span className='text-orange-500'>${price}</span></h3>
                 <div class="card-actions w-full">
-                    <button class="btn btn-primary w-full">Add to Cart</button>
+                    <button class="btn btn-primary w-full font-bold">Add to Cart</button>
                 </div>
-
-
-
 
                 <input type="checkbox" id="description-modal" class="modal-toggle" />
                 <div class="modal modal-bottom sm:modal-middle">
                     <div class="modal-box">
-                        <h3 class="font-bold text-lg">Congratulations random Interner user!</h3>
-                        <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                        <h3 class="font-bold text-lg">Description!</h3>
+                        <p class="py-4">{description}</p>
                         <div class="modal-action">
-                            <label for="description-modal" class="btn">Yay!</label>
+                            <label for="description-modal" class="btn bg-red-700">Close</label>
                         </div>
                     </div>
                 </div>
