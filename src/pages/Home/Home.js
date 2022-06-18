aa8d2e327c090e5ccf766237d367e82dbf888dee
unknown <coder.rakibul@gmail.com>
2022-06-18 16:55:53 +0600

    home page product added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Loading from '../Shared/Loading';
+import HomeItem from './HomeItem';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [shoes, setShoes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const firstFour = shoes.slice(0, 4);
+
+    useEffect(() => {
+        setLoading(true)
+        fetch('http://localhost:5000/shoe')
+            .then(res => res.json())
+            .then(data => {
+                setShoes(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) {
+        return <Loading></Loading>
+    }
     return (
-        <div>
-            <h2>This is home page</h2>
+        <div className='m-8'>
+            <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
+                {
+                    firstFour.map(shoe => <HomeItem
+                        key={shoe._id}
+                        shoe={shoe}
+                    ></HomeItem>)
+                }
+            </div>
+            <h2 className='w-48 mx-auto rounded text-center py-2 m-8 text-2xl text-white font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'><Link to="/shop">BUY NOW</Link></h2>
         </div>
     );
 };
---
+++ src/pages/Home/HomeItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const HomeItem = ({ shoe }) => {
+    const { name, model, image, price, description } = shoe;
+    return (
+        <div class="card bg-base-100 shadow-xl">
+
+           
+            <figure class="px-10 pt-10 h-48">
+                <img className=' mx-auto' src={image} alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body mt-6">
+                <h2>Brand: {name}</h2>
+                <p>Code: {model}</p>
+                <h3 className='text-xl font-bold'>Price: <span className='text-orange-500'>${price}</span></h3>
+                <label for="description-modal" class="btn modal-button">Description</label>
+
+                <input type="checkbox" id="description-modal" class="modal-toggle" />
+                <div class="modal modal-bottom sm:modal-middle">
+                    <div class="modal-box">
+                        <h3 class="font-bold text-lg">Description!</h3>
+                        <p class="py-4">{description}</p>
+                        <div class="modal-action">
+                            <label for="description-modal" class="btn bg-red-700">Close</label>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default HomeItem;
