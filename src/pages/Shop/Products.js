5792cc47a6ca9f8a7e38605ab3abaa6db2eba7ca
unknown <coder.rakibul@gmail.com>
2022-06-20 17:34:47 +0600

    pagination added

--- src/pages/Shop/Products.js
+++ src/pages/Shop/Products.js
@@ -9,24 +9,25 @@ const Products = () => {
     const [cart, setCart] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [page, setPage] = useState(0);
+    const [size, setSize] = useState(10);
+
+    useEffect(() => {
+        setLoading(true)
+        fetch(`http://localhost:5000/product?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+                setProducts(data)
+                setLoading(false)
+            })
+    }, [page, size]);
 
     useEffect(() => {
         fetch('http://localhost:5000/productCount')
             .then(res => res.json())
             .then(data => {
                 const count = data.count;
-                const pages = Math.ceil(count / 8);
+                const pages = Math.ceil(count / 10);
                 setPageCount(pages);
-            })
-    }, []);
-
-    useEffect(() => {
-        setLoading(true)
-        fetch('http://localhost:5000/product')
-            .then(res => res.json())
-            .then(data => {
-                setProducts(data)
-                setLoading(false)
             })
     }, []);
 
@@ -43,7 +44,7 @@ const Products = () => {
 
     return (
         <div className='p-4'>
-            <h2 className='text-center text-2xl font-bold my-8'>Product Collection: {products.length}</h2>
+            <h2 className='text-center text-2xl font-bold my-8'>Total: {products.length}</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
                 {
                     products.map(product => <Product
@@ -57,6 +58,12 @@ const Products = () => {
                 [...Array(pageCount).keys()]
                     .map(number => <button onClick={() => setPage(number)} className={page === number ? 'btn btn-active btn-primary mx-2 my-8' : 'btn btn-sm mx-2 my-8'}>{number}</button>)
             }
+            <select className='btn btn-sm mx-2' onChange={e => setSize(e.target.value)}>
+                <option value="5">5</option>
+                <option value="10" selected>10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+            </select>
         </div>
     );
 };
