4f2326820e6b4fdef91400fec090ffa3009327ed
unknown <coder.rakibul@gmail.com>
2022-06-21 01:48:45 +0600

    header updated

--- src/pages/Shared/Header.js
+++ src/pages/Shared/Header.js
@@ -44,17 +44,15 @@ const Header = ({ children }) => {
     const menuItems = <>
         <li><NavLink className='rounded-lg font-bold text-white' to="/home">Home</NavLink></li>
         <li><NavLink className='rounded-lg font-bold text-white' to="/shop">Shop</NavLink></li>
+        <li><NavLink className='rounded-lg font-bold text-white p-0 m-0' to="/cart">  <label tabIndex="0" className="btn btn-ghost">
+            <div className="indicator">
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                <span className="badge badge-sm indicator-item text-white">{cart.length}</span>
+            </div>
+        </label></NavLink></li>
         {
-            user ? <>
-                <li><NavLink className='rounded-lg font-bold text-white p-0 m-0' to="/cart">  <label tabIndex="0" className="btn btn-ghost">
-                    <div className="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                        <span className="badge badge-sm indicator-item text-white">{cart.length}</span>
-                    </div>
-                </label></NavLink></li>
+            user ?
                 <li><NavLink to="/login" onClick={logout} className='rounded-lg font-bold text-white'>Signout</NavLink></li>
-
-            </>
                 : <li><NavLink to="/login" className='rounded-lg font-bold text-white'>Login</NavLink></li>
         }
     </>
