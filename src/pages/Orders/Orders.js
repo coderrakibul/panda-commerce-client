a127747f654f67eec460daefe28e393192e59820
unknown <coder.rakibul@gmail.com>
2022-06-22 14:38:34 +0600

    conditional table created

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -22,36 +22,40 @@ const Orders = () => {
 
     return (
         <div className='container mx-auto mb-16'>
-            <h2 className='text-center text-2xl font-bold my-8'>Total: {orders.length}</h2>
+            {
+                orders.length > 0 ? <h2 className='text-center text-2xl font-bold my-8'>Total: {orders.length}</h2> : <h2 className='text-center text-2xl font-bold my-8'>No Items Added</h2>
+            }
             <div class="overflow-x-auto">
-                <table class="table w-full md:w-5/6 lg:w-3/5 mx-auto">
-                    <thead>
-                        <tr>
-                            <th>Number</th>
-                            <th>Image</th>
-                            <th>Model</th>
-                            <th>Total</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            orders.map((order, index) => <Order
-                                key={order._id}
-                                order={order}
-                                index={index}
-                            ></Order>)
-                        }
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td className='font-bold'>Total Pay =</td>
-                            <td className='font-bold text-xl'>{}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                {
+                    orders.length > 0 && <table class="table w-full md:w-5/6 lg:w-3/5 mx-auto">
+                        <thead>
+                            <tr>
+                                <th>Number</th>
+                                <th>Image</th>
+                                <th>Model</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                orders.map((order, index) => <Order
+                                    key={order._id}
+                                    order={order}
+                                    index={index}
+                                ></Order>)
+                            }
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td className='font-bold'>Total Pay =</td>
+                                <td className='font-bold text-xl'>{ }</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                }
             </div>
 
-            <button className="btn bg-green-500 btn-sm font-bold w-2/4 mx-auto block mt-8">Proceed Order</button>
+            {orders.length > 0 && <button className="btn bg-green-500 btn-sm font-bold w-2/4 mx-auto block mt-8">Proceed Order</button>}
         </div>
     );
 };
