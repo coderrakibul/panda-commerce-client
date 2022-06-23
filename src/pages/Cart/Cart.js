135eac0b14d549e19a1a9670dc168ca45d4b2c99
unknown <coder.rakibul@gmail.com>
2022-06-23 17:55:13 +0600

    calculation complete with tax and quantity

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
@@ -7,19 +7,30 @@ import Loading from '../Shared/Loading';
 const Cart = ({ cart, removeFromCart }) => {
     const { name, model, _id, image, price, quantity } = cart;
     const [user, loading] = useAuthState(auth);
+    const [count, setCount] = useState(1);
+    const newPrice = price * count;
 
     let totalQuantity = 0;
     let totalPrice = 0;
-    const shipping = 5;
+    const shipping = 5 * count;
 
     totalQuantity = quantity + totalQuantity;
-    totalPrice = price * totalQuantity;
-    const tax = parseFloat(price * 0.1).toFixed(2);
-    
+    totalPrice = newPrice * totalQuantity;
+    const tax = parseFloat(newPrice * 0.1).toFixed(2);
+
     const finalPrice = Math.round(totalPrice + shipping + parseFloat(tax));
 
+    const decrease = (id) => {
+        const totalQuantity = count - 1;
+        setCount(totalQuantity)
+    }
 
-    const handleAddToCart = (id) => {
+    const increase = (id) => {
+        const totalQuantity = count + 1;
+        setCount(totalQuantity)
+    }
+
+    const handleAddToOrder = (id) => {
         const order = {
             productId: cart._id,
             name,
@@ -66,13 +77,13 @@ const Cart = ({ cart, removeFromCart }) => {
                     <h3>Name: {name}</h3>
                     <h3>Model: {model}</h3>
                     <h3>Id: <span className='font-bold text-orange-500'>{_id}</span></h3>
-                    <h3 className='font-bold'>Price: <span className='text-orange-500'> ${price}</span> </h3>
+                    <h3 className='font-bold'>Price: <span className='text-orange-500'> ${newPrice}</span> </h3>
                 </div>
             </div>
 
             <div className='flex justify-center items-center gap-5 my-2'>
                 <div>
-                    <h3>Quantity: <button className='btn btn-ghost text-3xl font-bold text-red-500'>-</button><strong className='text-2xl'>{quantity}</strong> <button className='btn btn-ghost text-3xl font-bold text-green-500'>+</button></h3>
+                    <h3>Quantity: <button onClick={() => decrease(_id)} className='btn btn-ghost text-3xl font-bold text-red-500'>-</button><strong className='text-2xl'>{count}</strong> <button onClick={() => increase(_id)} className='btn btn-ghost text-3xl font-bold text-green-500'>+</button></h3>
                     <h3>Shipping: ${shipping}</h3>
                     <h3>TAX- 10%: ${tax}</h3>
                     <h3 className='font-bold'>Total Price: <span className='text-orange-500'> ${finalPrice}</span></h3>
@@ -81,7 +92,7 @@ const Cart = ({ cart, removeFromCart }) => {
                 <div className='flex flex-col gap-2'>
                     <button onClick={() => removeFromCart(cart._id)} className="btn bg-red-600 btn-sm font-bold">Delete</button>
 
-                    <button onClick={() => handleAddToCart(_id)} className="btn btn-sm font-bold">Order</button>
+                    <button onClick={() => handleAddToOrder(_id)} className="btn btn-sm font-bold">Order</button>
 
                 </div>
             </div>
