98dffb81a7e273223978568b18981a5c9ad3cd51
unknown <coder.rakibul@gmail.com>
2022-06-23 14:47:10 +0600

    orders post to database

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,12 +1,45 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
+import auth from '../../firebase.init';
+import Loading from '../Shared/Loading';
 
 const Cart = ({ cart, removeFromCart }) => {
     const { name, model, _id, image, price, quantity } = cart;
+    const [user, loading] = useAuthState(auth);
 
     const totalPrice = price * quantity;
     const shipping = 5;
     const tax = parseFloat(totalPrice / 100 * 10).toFixed(2);
     const finalPrice = Math.round(totalPrice + shipping + parseFloat(tax));
+
+    const handleAddToCart = (id) => {
+        const order = {
+            productId: cart._id,
+            name,
+            model,
+            user: user.email,
+            price,
+            image
+        }
+
+        fetch('http://localhost:5000/order', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+
+    }
+
+    if (loading) {
+        return <Loading></Loading>
+    }
 
 
     return (
@@ -35,7 +68,7 @@ const Cart = ({ cart, removeFromCart }) => {
                 <div className='flex flex-col gap-2'>
                     <button onClick={() => removeFromCart(cart._id)} className="btn bg-red-600 btn-sm font-bold">Delete</button>
 
-                    <button className="btn btn-sm font-bold">Order</button>
+                    <button onClick={() => handleAddToCart(_id)} className="btn btn-sm font-bold">Order</button>
 
                 </div>
             </div>
