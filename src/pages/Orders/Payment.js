2a46facc2598995081b4bcda5869870f87d25c13
unknown <coder.rakibul@gmail.com>
2022-06-24 16:27:59 +0600

    payment success

--- src/pages/Orders/Payment.js
+++ src/pages/Orders/Payment.js
@@ -4,6 +4,7 @@ import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 
 
@@ -15,13 +16,19 @@ const Payment = () => {
 
     const [product] = useState({
         name: 'Product',
-        price: 300,
+        price: 500,
     })
 
     async function handleToken(token, addresses) {
         const response = await axios.post('http://localhost:5000/checkout', { token, product })
+        console.log(response.status);
+        if (response.status === 200) {
+            toast('Payment Success', { type: 'success' })
+        }
+        else {
+            toast.error('Payment Failed', { type: 'failed' })
+        }
 
-        console.log(response.status)
     }
 
     useEffect(() => {
@@ -42,6 +49,9 @@ const Payment = () => {
         <div>
             <h2 className='text-center text-2xl font-bold my-8'> Please Pay to Purchase! </h2>
             <div className='text-center'>
+                <h2 className="text-center">Product Info</h2>
+                <h3 className="text-center">Product Name: {product.name}</h3>
+                <h3 className="text-center">Product Price: {product.price}</h3>
                 <StripeCheckout
                     stripeKey='pk_test_51L0gwOCHgcol8ks1TUlNWNgW47HRGblYDdW2auw0wud0KWinkGaNs7d6XeEXOKsuxNbVchYWNnFZ8Pq40lumBq1300tAFEvpgI'
                     amount={product.price * 100}
