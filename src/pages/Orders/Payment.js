5b7488cedcd9e21587858db50e800a31e00bdd06
unknown <coder.rakibul@gmail.com>
2022-06-26 00:48:49 +0600

    hero card

--- src/pages/Orders/Payment.js
+++ src/pages/Orders/Payment.js
@@ -22,8 +22,8 @@ const Payment = () => {
         <div className='mx-auto'>
             <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
                 <div class="card-body">
-                    <p className="text-success font-bold">Hello, {order.userName}</p>
-                    <h2 class="card-title">Please Pay <span>${order.finalPrice} for </span> {order.model}</h2>
+                    <h2>Hello, <span className="text-success font-bold text-xl">{order.userName}</span></h2>
+                    <h2 class="card-title">Please Pay <span className='text-orange-500 font-bold'>${order.finalPrice} </span> for {order.model}</h2>
                 </div>
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
