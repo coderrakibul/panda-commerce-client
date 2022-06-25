87fbb2d1e6e1775b5c9cd9e35f93da84bbe7d979
unknown <coder.rakibul@gmail.com>
2022-06-26 02:50:18 +0600

    payment process ready

--- src/pages/Orders/Payment.js
+++ src/pages/Orders/Payment.js
@@ -1,7 +1,13 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+
+
+const stripePromise = loadStripe('pk_test_51L0gwOCHgcol8ks1TUlNWNgW47HRGblYDdW2auw0wud0KWinkGaNs7d6XeEXOKsuxNbVchYWNnFZ8Pq40lumBq1300tAFEvpgI');
 
 const Payment = () => {
     const { id } = useParams();
@@ -28,7 +34,9 @@ const Payment = () => {
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm order={order} />
+                    </Elements>
                 </div>
             </div>
         </div>
