7e8e807cf197950026a0ed406e8bd2d1d9640f4f
unknown <coder.rakibul@gmail.com>
2022-06-26 21:45:03 +0600

    live url added

--- src/pages/Orders/CheckoutForm.js
+++ src/pages/Orders/CheckoutForm.js
@@ -13,7 +13,7 @@ const CheckoutForm = ({ order }) => {
     const { _id, user, userName, finalPrice } = order;
 
     useEffect(() => {
-        fetch('http://localhost:5000/create-payment-intent', {
+        fetch('https://polar-lake-06561.herokuapp.com/create-payment-intent', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json',
@@ -79,7 +79,7 @@ const CheckoutForm = ({ order }) => {
                 product: _id,
                 transactionId: paymentIntent.id
             }
-            fetch(`http://localhost:5000/order/${_id}`, {
+            fetch(`https://polar-lake-06561.herokuapp.com/order/${_id}`, {
                 method: 'PATCH',
                 headers: {
                     'content-type': 'application/json',
--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -12,7 +12,7 @@ const Orders = () => {
     useEffect(() => {
         if (user) {
             setLoading(true);
-            fetch(`http://localhost:5000/order?user=${user.email}`)
+            fetch(`https://polar-lake-06561.herokuapp.com/order?user=${user.email}`)
                 .then(res => res.json())
                 .then(data => setOrders(data));
             setLoading(false);
@@ -23,7 +23,7 @@ const Orders = () => {
     const removeFromOrder = (id) => {
         const proceed = window.confirm('are you sure?');
         if (proceed) {
-            const url = `http://localhost:5000/order/${id}`;
+            const url = `https://polar-lake-06561.herokuapp.com/order/${id}`;
             fetch(url, {
                 method: 'DELETE'
             })
