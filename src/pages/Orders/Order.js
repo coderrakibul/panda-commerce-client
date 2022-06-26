4698ef3421fa5a7a2eee67f364a04a1534d0ac74
unknown <coder.rakibul@gmail.com>
2022-06-26 21:58:32 +0600

    pay button update

--- src/pages/Orders/Order.js
+++ src/pages/Orders/Order.js
@@ -12,7 +12,7 @@ const Order = ({ order, index, removeFromOrder }) => {
                 <button onClick={() => removeFromOrder(_id)} className="btn btn-circle bg-red-500 ml-8 btn-xs">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
                 </button>
-                {!order?.paid && <Link to={`/payment/${_id}`}><button className='text-green-500 font-bold ml-4 btn btn-ghost btn-sm'>Pay</button></Link>}
+                {!order?.paid && <Link to={`/payment/${_id}`}><button className='text-red-500 font-bold ml-4 btn btn-ghost btn-sm'>Pay</button></Link>}
 
                 {order?.paid && <span className='text-green-500 font-bold ml-4'>Paid</span>}
 
